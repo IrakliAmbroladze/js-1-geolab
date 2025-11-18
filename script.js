1439ac87e6abe6e2f87c7e68ed3b4e2977f35617
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-19 01:19:21 +0400

    feat: add prompt for user firstname, lastname and age

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ console.log("This is an undefined: ", undefinedVariable);
 
 const nullVariable = null;
 console.log("This is a null: ", nullVariable);
+
+const firstName = prompt("დაწერე შენი სახელი");
+const lastName = prompt("დაწერე შენი გვარი");
+const age = prompt("დაწერე შენი ასაკი");
+console.log("მე ვარ", firstName, lastName, "და ვარ", age, "წლის.");
