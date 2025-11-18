64b2e6378525a24c5ffc898e6cb6e306381dda1d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-19 01:01:41 +0400

    feat: create variables for string, number, boolean, undefined and null

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-(function myFunction() {
-  document.getElementById("demo").innerText = "Hello World";
-})();
+const stringVariable = "I am a string";
+console.log("This is a string: ", stringVariable);
+
+const numberVariable = 10;
+console.log("This is a number: ", numberVariable);
+
+const booleanVariable = true;
+console.log("This is a boolean: ", booleanVariable);
+
+let undefinedVariable;
+console.log("This is an undefined: ", undefinedVariable);
+
+const nullVariable = null;
+console.log("This is a null: ", nullVariable);
